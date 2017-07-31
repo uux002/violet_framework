236b5b726de05350e3f2948b45ac07ddc1cac535
uux002 <uux002@gmail.com>
2017-07-31 08:50:31 +0800

    Add Some patterns

---
+++ Assets/VioletFramework/Patterns/Command/Command.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class Command
+{
+    public Command(){
+        
+    }
+
+    public virtual void Execute(){
+        
+    }
+
+    public virtual void Execute(GameActor _actor){
+        
+    }
+}
